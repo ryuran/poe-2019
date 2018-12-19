1a0421cd436c83517275d85c10ef236272e2b6cc
Alan Mahjoub <alan.mahjoub@gmail.com>
2018-12-19 15:01:01 +0100

    added responsive nav

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
 require('./style.scss');
 console.log('Hello World');
+
+const responsiveNav = require('responsive-nav');
+require('responsive-nav/client/dist/styles/responsive-nav.css');
+
+const nav = responsiveNav('nav');
