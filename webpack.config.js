2be6421ffea3b0f38056fcc45d3eb40b7af66721
Laurent Boulanger <pro@laurentboulanger.com>
2018-12-19 12:31:06 +0100

    Install webpack

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin'); //installed via npm
+
+module.exports = {
+    entry: './src/index.js',
+    mode: 'development',
+    plugins: [
+      new HtmlWebpackPlugin({template: './src/index.html'})
+    ]
+  };
